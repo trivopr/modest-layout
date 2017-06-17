f664b412a960c13f69feb32f76133d7c7379e3f2
Minhtri <minhtripr1605@gmail.com>
2017-06-18 01:19:39 +0700

    update

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,13 +1,18 @@
-$(function(){
-   
-    $('section[data-type="bg"]').each(function(){
-        var $this=$(this);
-        var $window=$(window);
-        $window.scroll(function(){
-            var y=-($window.scrollTop()/($this.data('speed')/2));
-            var position="100% "+y+"px";
-            $this.css({backgroundPosition:position});
-            
+$(document).ready(function(){
+
+        $('section[data-type="bg"]').each(function(){
+            var $this=$(this);
+            var $window=$(window);
+            $window.scroll(function(){
+                var y=-($window.scrollTop()/($this.data('speed')/2));
+                var position="100% "+y+"px";
+                $this.css({backgroundPosition:position});
+                
+            });
         });
-    });
+        $(".image-member").hover(function(){
+                $(this).children( ".personal" ).slideDown(500);                
+            }, function(){
+                $(this).children( ".personal" ).slideUp(500);
+        });
 });
